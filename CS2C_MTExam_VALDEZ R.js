47b68bc981df70c59da8af39fbcbca27633e6bf8
VALDEZ R. <renzv753@gmail.com>
2024-10-30 17:26:52 +0800

    Create CS2C_MTExam_VALDEZ R.js

---
+++ CS2C_MTExam_VALDEZ R.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures & Algorithms";
+let classSchedule = "Monday, Tuesday, Thursday, Friday 10:00 AM - 11:00 AM, 8:00 - 9:30";
+let classroom = "CLB";
+let classInstructor = "Clifford Owen Pascua";
+let studentName = "Renz Valdez";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
